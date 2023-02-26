e79b057ebf02296ba7ca70ec34d7ac439cb54176
wiktor-falek <falekwiktor@gmail.com>
2023-02-26 17:46:55 +0100

    Start implementing sse.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import cron from "node-cron";
 
 const app = express();
+
+app.use(cors());
 
 // TODO #2: encapsulate inside a class that will store and load the data
 const currentLeague = "Sanctum";
@@ -26,7 +30,53 @@ app.get("/currency", async (req: Request, res: Response) => {
   res.send({ mirror: currencyData.lines[0], divine: currencyData.lines[8] });
 });
 
+interface Client {
+  id: number;
+  response: Response;
+}
+let clients: Array<Client> = [];
+app.get("/events", function eventsHandler(req, res) {
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+  };
+  res.writeHead(200, headers);
+
+  res.write(`data: hello\n\n`);
+
+  const clientId = Date.now();
+
+  const newClient: Client = {
+    id: clientId,
+    response: res,
+  };
+
+  clients.push(newClient);
+
+  console.log(`${clientId} Connection open`);
+
+  req.on("close", () => {
+    console.log(`${clientId} Connection closed`);
+    clients = clients.filter((client) => client.id !== clientId);
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening at port ${PORT}`);
+});
+
+cron.schedule("*/1 * * * *", () => {
+  // check if data updated
+  const updated = false;
+  if (updated) {
+    console.log("Data updated");
+  }
+  // fetch the data and update if success
+  // emit updated data (or the endpoint and have the client fetch it?) to each client
+  for (const client of clients) {
+    // const data = JSON.stringify()
+    // client.response.write("data: " + data + "\n\n");
+  }
 });
